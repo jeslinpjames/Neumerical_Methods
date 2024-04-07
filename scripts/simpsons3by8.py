ed3cb6dc23352a4581b991157ae2a5595812dbb8
jeslinpjames <jeslinpjames007@gmail.com>
2024-04-07 23:46:55 +0530

    completed simpsons 3/8

---
+++ scripts/simpsons3by8.py
@@ -0,0 +1,17 @@
+import numpy as np
+def simpsons3by8(x,y):
+    h = x[1]-x[0]
+    n = len(x)
+    sum = y[0] + y[n-1]
+    for i in range(1,n-1):
+        if(i % 3 == 0):
+            sum += y[i]*2
+        else:
+            sum+= y[i]*3
+    return (3*sum*h)/8
+
+x_data = np.array([0, 1/6, 1/3, 1/2, 2/3, 5/6, 1])
+y_data = np.array([1, 0.9729, 0.9, 0.8, 0.6923, 0.59016, 1/2])
+result = simpsons3by8(x_data,y_data)
+print(result)
+
