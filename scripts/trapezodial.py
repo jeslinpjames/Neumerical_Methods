22502f9eac2722aa640e3a138473165c85996a38
jeslinpjames <jeslinpjames007@gmail.com>
2024-04-07 23:58:39 +0530

    trapezodial added

---
+++ scripts/trapezodial.py
@@ -0,0 +1,14 @@
+import numpy as np
+def trapezodial(x,y):
+    h = x[1]-x[0]
+    n = len(x)
+    sum = y[0] + y[n-1]
+    for i in range(1,n-1):
+       sum += y[i] * 2
+    return (sum*h)/2
+
+x_data = np.array([0, 1/2, 1, 3/2, 2])
+y_data = np.array([1, 0.6064, 0.3674, 0.2231, 0.1353])
+result = trapezodial(x_data,y_data)
+print(result)
+
